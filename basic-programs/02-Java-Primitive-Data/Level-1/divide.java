ea9d3ff3b6ba79291b80f96bc63d4f87e5fee83d
Kushagra28P <pandeyji6882@gmail.com>
2026-02-10 18:37:36 +0530

    [Kushgara pandey] arrays files added

--- basic-programs/02-Java-Primitive-Data/Level-1/divide.java
+++
@@ -1,7 +0,0 @@
-public class divide{
-public static void main(String[] args) {
-int a = 14/3;
-int b = 14%3;
-System.out.println("The Pen Per student is " +a+" and the remaining pen not distributed is "+b);
-}
-}
